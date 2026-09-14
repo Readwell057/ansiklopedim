0e5fe71581b6c79e4456686117a7a0c1f5cf5f9b
Readwell057 <Readwell42@gmail.com>
2026-09-14 09:21:02 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ansiklopedim-cache-v1';
+const CACHE_NAME = 'ansiklopedim-cache-v2';
 const FILES_TO_CACHE = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
